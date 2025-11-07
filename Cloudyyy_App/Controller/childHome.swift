b3b323cb06cee0bbdd6055e0bd94e64037702612
joneslinso <jl9703@srmist.edu.in>
2025-11-07 10:21:15 +0530

    childHome updated

--- Cloudyyy_App/Controller/childHome.swift
+++ Cloudyyy_App/Controller/childHome.swift
@@ -4,6 +4,47 @@ class childHome: UIViewController {
 
     @IBOutlet weak var bgsamplegrad: UIView!
 
+    @IBOutlet weak var lblGreeting: UILabel!
+    
+    @IBOutlet weak var lblSubGreeting: UILabel!
+    
+    @IBOutlet weak var btnBell: UIButton!
+    
+    @IBOutlet weak var btnProfile: UIButton!
+    
+    
+    @IBOutlet weak var mascotContainerView: UIView!
+    
+    @IBOutlet weak var speechBubbleView: UIView!
+    
+    @IBOutlet weak var lblSpeech: UILabel!
+    
+    @IBOutlet weak var imgCloudMain: UIImageView!
+    
+    @IBOutlet weak var powerCardView: UIView!
+    
+    @IBOutlet weak var powerLblStackView: UIStackView!
+    
+    @IBOutlet weak var imgCloudguitarSmall: UIImageView!
+    
+    @IBOutlet weak var lblQuotesLabel: UILabel!
+    
+    
+    @IBOutlet weak var lblPowerView: UILabel!
+    
+    
+    @IBOutlet weak var lblAchievementsTitle: UILabel!
+    
+    @IBOutlet weak var achievementsStack: UIStackView!
+    
+    @IBOutlet weak var imgHabitsCloud: UIImageView!
+    
+    @IBOutlet weak var lblHabit: UILabel!
+    
+    @IBOutlet weak var habitprogressView: UIProgressView!
+    
+    
+
     // Keep a reference to the gradient layer
     private var gradientLayer: CAGradientLayer?
 
